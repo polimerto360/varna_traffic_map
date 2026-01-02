35583b8c6dce58e3a84fdb4d74b71523d7f7a8da
polimerto360 <ivan@kolevi.net>
2026-01-02 15:21:30 +0200

    fixed local repo + added estimation for residents, employees + base sim classes

--- traffic_map.cpp
+++ traffic_map.cpp
@@ -2,47 +2,81 @@
 //
 
 #include "traffic_map.h"
-#include <windows.h>
+
 
 #pragma execution_character_set("utf-8")
 
 using namespace std;
 using namespace geodesk;
+using namespace traffic_sim;
+using namespace config;
 
 static ostream& operator<<(ostream& out, const Coordinate& coord)
 {
-	out << "(" << coord.x << ", " << coord.y << ")";
+	out << "(" << Mercator::latFromY(coord.y) << ", " << Mercator::lonFromX(coord.x) << ")";
 	return out;
 }
 
 int main()
 {
+
+
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 	SetConsoleOutputCP(65001); // CP_UTF8
-
+	cout << setprecision(9);
 	cout << "Hello CMake." << endl;
 
 	Features features("D:\\storage\\traffic_map\\traffic_map\\bulgaria.gol");
-	Feature varna = features("a[admin_level=5][name:en=Varna]").one();
-	Features features_in_varna = features.intersecting(varna);
+	Feature varna = features("a[admin_level=5][name:en=Varna]").one(); // whole varna
+	Features features_in_varna = features.within(varna);
 
+
+	// temp for testing in vuzrazhdane
+	/*
+	const Feature* vuzrazhdane;
+	
+	for (const Feature& f : features_in_varna) {
+		if (f.id() == 16951190) { vuzrazhdane = &f; break; }
+	}
+	features_in_varna = features_in_varna.intersecting(*vuzrazhdane);
+	*/
 	Ways roads = features_in_varna.ways("[highway=motorway,trunk,primary,secondary,tertiary,residential,road]");
 	Relations bus_routes = features_in_varna.relations("r[route=bus][type=route]");
 	Ways residential_areas = features_in_varna.ways("a[landuse=residential]");
-	Ways workplaces = features_in_varna.ways("a[building=office,industrial]");
-	Nodes workplace_nodes = features_in_varna.nodes("[shop],[amenity][amenity!=bicycle_parking,bicycle_repair_station,bicycle_rental,bicycle_wash,bus_station,compressed_air,charging_station,driver_training,grit_bin,motorcycle_parking,parking,parking_entrance,parking_space,taxi,weighbridge,atm,payment_terminal,baby_hatch,fountain,stage,studio,post_box,bbq,bench,check_in,dog_toilet,dressing_room,drinking_water,give_box,lounge,mailroom,parcel_locker,shelter,shower,telephone,toilets,water_point,watering_place,sanitary_dump_station,recycling,waste_basket,waste_disposal,baking_oven,clock,grave_yard,hunting_stand,kitchen,kneipp_water_cure,lounger,photo_booth,place_of_mourning,place_of_worship,public_bath,vending_machine,hydrant]");
 	Ways parkings = features_in_varna.ways("a[amenity=parking]");
+	
+	//Features workplace_features = features_in_varna("a[disused:shop=mall]");
+	//Features workplace_features = features_in_varna("a[shop=mall]");
+	//Nodes workplace_nodes = features_in_varna.nodes("[shop],[amenity][amenity!=bicycle_parking,bicycle_repair_station,bicycle_rental,bicycle_wash,bus_station,compressed_air,charging_station,driver_training,grit_bin,motorcycle_parking,parking,parking_entrance,parking_space,taxi,weighbridge,atm,payment_terminal,baby_hatch,fountain,stage,studio,post_box,bbq,bench,check_in,dog_toilet,dressing_room,drinking_water,give_box,lounge,mailroom,parcel_locker,shelter,shower,telephone,toilets,water_point,watering_place,sanitary_dump_station,recycling,waste_basket,waste_disposal,baking_oven,clock,grave_yard,hunting_stand,kitchen,kneipp_water_cure,lounger,photo_booth,place_of_mourning,place_of_worship,public_bath,vending_machine,hydrant]");
+	map<int64_t, vector<building>> residential_buildings; // map of residential area id to buildings in that area
 
 	int count = 0;
 
-	for (const Node& node : workplace_nodes)
-	{
-		count++;
-		
-		cout << "workplace: " << node["name"] << "; pos: " << node.xy() << endl;
+	for (const Way& a : residential_areas) {
+		cout << "Residential Area: " << a["name"] << endl;
+		Ways res_areas = features_in_varna.ways("a[building=residential,house,apartments,detached,terrace]").intersecting(a);
+		for (const Way& f : res_areas) {
+			building b(f, building::RESIDENTIAL, ((f["building"] == "house") ? 0.002 : 0.0)); // lower density for houses
+			residential_buildings[a.id()].push_back(b);
+			cout << "Residential Building: " << b.name << ", Capacity: " << b.capacity << ", Location: " << b.location->centroid() << endl;
+			count += b.capacity;
+		}
 	}
-	cout << "Total workplace: " << count << endl;
+	cout << "Total residents: " << count << endl;
+
+	Features workplace_features = features_in_varna("a[disused:shop=mall],a[building=office,industrial,school,kindergarten],a[shop],n[shop],n[amenity][amenity!=bicycle_parking,bicycle_repair_station,bicycle_rental,bicycle_wash,bus_station,compressed_air,charging_station,driver_training,grit_bin,motorcycle_parking,parking,parking_entrance,parking_space,taxi,weighbridge,atm,payment_terminal,baby_hatch,fountain,stage,studio,post_box,bbq,bench,check_in,dog_toilet,dressing_room,drinking_water,give_box,lounge,mailroom,parcel_locker,shelter,shower,telephone,toilets,water_point,watering_place,sanitary_dump_station,recycling,waste_basket,waste_disposal,baking_oven,clock,grave_yard,hunting_stand,kitchen,kneipp_water_cure,lounger,photo_booth,place_of_mourning,place_of_worship,public_bath,vending_machine,hydrant]");
+	vector<building> workplaces;
+
+	count = 0;
+	for (const Feature& f : workplace_features)
+	{
+		building b(f, building::WORKPLACE);
+		workplaces.push_back(b);
+		cout << "Workplace: " << b.name << ", Capacity: " << b.capacity << ", Location: " << b.location->centroid() << endl;
+		count += b.capacity;
+	}
+	cout << "Total positions: " << count << endl;
 	return 0;
 }
--- traffic_map.h
+++ traffic_map.h
@@ -5,4 +5,142 @@
 
 #include <iostream>
 #include <geodesk/geodesk.h>
+#include <windows.h>
+#include <vector>
+#include <map>
+#include <tuple>
+#include <algorithm>
+#include <cmath>
 // TODO: Reference additional headers your program requires here.
+
+namespace config {
+	const double TIME_STEP = 1.0; // time step in seconds
+	const double DEFAULT_BUILDING_HEIGHT = 5.0; // default building height in meters
+	// target workplaces is 150000
+	const double EMPLOYEE_DENSITY = 0.024; // people per cubic meter (adding 0.01 increases result by 56309)
+	const int MIN_EMPLOYEES = 5; // minimum people per workplace (adds or subtracts 2861)
+
+	// target residents is 350000
+	const double RESIDENTIAL_DENSITY = 0.012;
+	const int MIN_RESIDENTS = 3;
+
+	const double ENTERTAINMENT_DENSITY = 0.024; 
+	const int MIN_VISITORS = 5; 
+	
+
+	//const int NUM_CARS = 1000; // total number of cars in the simulation
+}
+
+namespace traffic_sim
+{
+	using namespace std;
+	using namespace geodesk;
+	using namespace config;
+
+	struct segment;
+	struct person; // forward declaration
+	struct workplace; // forward declaration
+	struct car; // forward declaration
+
+	struct person {
+		string name;
+		int age;
+		bool can_drive;
+		Feature* home;
+
+		workplace* work; // or any place that is visited regularly
+
+		person(string n, int a, bool cd, Feature* h, workplace* w) {
+			name = n;
+			age = a;
+			can_drive = cd;
+			home = h;
+			work = w;
+		}
+	};
+	struct segment { // directed road segment
+		Node start;
+		Node end;
+		double length() {
+			return sqrt(pow(end.x() - start.x(), 2) + pow(end.y() - start.y(), 2));
+		}
+		vector<car*> cars;
+		double max_speed; // maximum speed allowed on the segment
+	};
+	struct car {
+		Coordinate position;
+		double cur_speed;
+		double max_speed;
+		segment* cur_segment;
+
+		person* driver;
+
+		car(Coordinate pos, double max_sp, segment seg, person dr) {
+			position = pos;
+			cur_speed = 0.0;
+			max_speed = max_sp;
+			cur_segment = &seg;
+			driver = &dr;
+		}
+	};
+
+	struct building {
+		static enum building_type {
+			RESIDENTIAL,
+			WORKPLACE,
+			ENTERTAINMENT
+		};
+		string name;
+		building_type type;
+		Feature* location;
+		int capacity; // number of residents/people/employees
+		int cur_employees = 0;
+
+		building(Feature loc, building_type t, double people_density = 0.0) {
+			name = loc["name"];
+			type = t;
+			
+			if(name == "") {
+				if (t == RESIDENTIAL) name = "Unnamed Residential Building";
+				else if(t == ENTERTAINMENT) name = "Unnamed Entertainment Building";
+				else name = "Unnamed Workplace";
+			}
+
+			location = &loc;
+			double height = DEFAULT_BUILDING_HEIGHT;
+			
+			if(loc.hasTag("height")) {
+				height = stod(loc["height"]);
+			}
+
+			int min_cap;
+			switch (type) {
+				case RESIDENTIAL:
+					if (people_density == 0.0)
+						people_density = RESIDENTIAL_DENSITY; // residents per cubic meter
+					min_cap = MIN_RESIDENTS;
+					break;
+				case ENTERTAINMENT:
+					if (people_density == 0.0)
+						people_density = ENTERTAINMENT_DENSITY; // people per cubic meter
+					min_cap = MIN_VISITORS;
+					break;
+				case WORKPLACE:
+					if (people_density == 0.0)
+						people_density = EMPLOYEE_DENSITY; // employees per cubic meter
+					min_cap = MIN_EMPLOYEES;
+					break;
+			}
+
+			capacity = max((int)(height * loc.area() * people_density), min_cap);
+		}
+
+	};
+
+
+	struct bus : car {
+		int route_id;
+		vector<Coordinate> stops;
+		vector<person*> passengers;
+	};
+}
